2feaa424be5ab7323669c8c79a8a3e45b50e48fd
Jorge Mora <jmora2ec@gmail.com>
2023-04-04 19:16:53 -0500

    add colormode

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,9 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem bg="blue.200" area={"aside"}>
-          Aside
-        </GridItem>
+        <GridItem area={"aside"}>Aside</GridItem>
       </Show>
-      <GridItem bg="green.300" area={"main"}>
-        Main
-      </GridItem>
+      <GridItem area={"main"}>Main</GridItem>
     </Grid>
   );
 }
